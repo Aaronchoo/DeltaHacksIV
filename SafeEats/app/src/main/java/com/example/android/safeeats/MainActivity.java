318f06a2c388b44446ebcd08ddf63509040ffb24
Iconic <aaronchoo9@gmail.com>
2018-01-27 12:36:04 -0500

    Start Project

---
+++ SafeEats/app/src/main/java/com/example/android/safeeats/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.safeeats;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button allergy = (Button) findViewById(R.id.alle);
+        allergy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openAllergy = new Intent(MainActivity.this, allergies.class);
+                startActivity(openAllergy);
+            }
+        });
+    }
+}
