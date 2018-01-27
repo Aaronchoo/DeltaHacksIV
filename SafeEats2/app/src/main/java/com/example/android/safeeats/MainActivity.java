ea42e6288ddf48af52efbb61a774f3be9797f804
Alex Chan <alexchan150@gmail.com>
2018-01-27 16:25:16 -0500

    Got rid of button

--- SafeEats2/app/src/main/java/com/example/android/safeeats/MainActivity.java
+++ SafeEats2/app/src/main/java/com/example/android/safeeats/MainActivity.java
@@ -21,14 +21,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(openAllergy);
             }
         });
-
-        Button choosingChoice = (Button) findViewById(R.id.choosing_names);
-        choosingChoice.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent startChoices = new Intent(MainActivity.this, NameActivity.class);
-                startActivity(startChoices);
-            }
-        });
     }
 }
