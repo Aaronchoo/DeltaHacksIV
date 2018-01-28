c9eebe931b61cf0883c865aa95761113880d8a88
Iconic <aaronchoo9@gmail.com>
2018-01-28 06:29:43 -0500

    Trivial

---
+++ SafeEats2/app/src/main/java/com/example/android/safeeats/RecipeRequest.java
@@ -0,0 +1,14 @@
+package com.example.android.safeeats;
+
+/**
+ * Created by ChooA on 1/28/2018.
+ */
+
+public class RecipeRequest {
+    public final String key ="28878fecb7604f01b315811acc08949b";
+    public final String q;
+
+    public RecipeRequest(String q) {
+        this.q = q;
+    }
+}
